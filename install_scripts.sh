e7889cd774ef8f8d0b91376a3b0bce87f9004ec7
Blake Smith <blakeinvictoria@gmail.com>
2021-03-28 02:34:19 -0700

    Add `one_shot_install` to set of ignored scripts.

--- install_scripts.sh
+++ install_scripts.sh
@@ -1,5 +1,7 @@
 # Get all executible files in the current directory
-FILES=$(find . -type f -executable -follow -print | grep -v install_scripts.sh)
+FILES=$(find . -type f -executable -follow -print \
+	| grep -v install_scripts.sh \
+	| grep -v one_shot_install.sh)
 
 # Copy files to ~/.local/bin
 cp -r  $FILES  ~/.local/bin
