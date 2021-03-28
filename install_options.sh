84a31153ae37c4b9107715d752a1f4cb1537a509
Blake Smith <blakeinvictoria@gmail.com>
2021-03-28 03:21:09 -0700

    Add `install_options.sh`
    Prompt user for configurable options.

---
+++ install_options.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+echo -n "Where would you like to keep meta-data files? (defaults to ~/.scripts-meta):"
+read path
+
+path=${path:-"$HOME/.scripts-meta"}
+
+mkdir -p $path
+
+echo "SCRIPTS_META_DATA_PATH=$path" >> options.env
+
--- install_scripts.sh
+++ install_scripts.sh
@@ -1,3 +1,9 @@
+#!/bin/sh
+
+
+# Prompt user for install options
+./install_options.sh
+
 # Get all executible files in the current directory
 FILES=$(find . -type f -executable -follow -print \
 	| grep -v install_scripts.sh \
