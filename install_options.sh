2c1aa275b7cf3aeca23f007979e679520ee1d176
Blake Smith <blakeinvictoria@gmail.com>
2021-03-28 03:50:10 -0700

    Properly load env variables in bashrc.

--- install_options.sh
+++ install_options.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
 
 # Setup for envfile to be loaded
-command="export $(cat .env | xargs)"
+command="source $HOME/.scripts/options.env"
 grep -qxF "$command" "$HOME/.bashrc" || \
 	echo "$command" >> "$HOME/.bashrc"
 
@@ -16,4 +16,3 @@ path=${path:-"$HOME/.scripts-meta"}
 mkdir -p $path
 
 echo "SCRIPTS_META_DATA_PATH=$path" > options.env
-
