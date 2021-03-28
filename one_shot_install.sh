94f26235bd5a5ec2ffe67782990b90fdb42438b9
Blake Smith <blakeinvictoria@gmail.com>
2021-03-28 02:29:14 -0700

    Add script to install from github.

---
+++ one_shot_install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Clone and install scripts. Intended to be used with curl.
+
+tmp=$(mktemp -d -t .scipts-XXX)
+
+git clone https://github.com/BlakeASmith/.scripts
+
+cd .scripts
+./install_scripts.sh
+
+cd ..
+cp -r .scripts ~/.scripts
+
+rm -rf $tmp
